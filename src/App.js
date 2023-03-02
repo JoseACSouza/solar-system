82ddf7b24e0f75783bb2445efc3a0253fa07739f
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2023-03-02 08:38:41 -0300

    requisito 5 cumprido

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import SolarSystem from './components/SolarSystem';
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <Header />
         <SolarSystem
           headline="Planetas"
